0480f81e54689ce59e174be04ed72706d52e0245
floh135 <61746422+floh135@users.noreply.github.com>
2021-06-26 13:31:15 +0200

    Feed updated

--- src/components/PersonalFeed.js
+++ src/components/PersonalFeed.js
@@ -8,28 +8,21 @@ export default class PersonalFeed extends React.Component {
     }
 
     state = {
+        currentUser: {query: {match: { user: localStorage.getItem("user")}}},
         posts: [],
         counter: 0
     }
 
     componentDidMount() {
         console.log('Mount!')
-        axios.get('http://localhost:9200/postindex/post/_search', {
-            data: {
-                query: {
-                    match: {
-                        user: localStorage.getItem("user")
-                    }
-                },
-                sort: [
-                    {
-                        timestamp: "asc"
-                    }
-                ]
-            }
-        }).then(res => {
-            console.log('Personal Posts' + res.data);
+
+        console.log(JSON.stringify(this.state.currentUser));
+
+        axios.post('http://localhost:9200/postindex/post/_search', this.state.currentUser)
+        .then(res => {
+            console.log('Personal Posts' + res.data.hits.hits);
             const posts = res.data.hits.hits;
+            console.log(localStorage.getItem("user"));
 
             this.setState({ posts })
         }).catch(function (error)  {
@@ -44,20 +37,20 @@ export default class PersonalFeed extends React.Component {
         ev = this.state.posts
 
         return(
-            <table>
-                <tbody>
-                {ev.map((post) => {
-                    return (
-                        <tr>
-                            <td> {post._source.user}</td>
-                            <td> {post._source.emotion}</td>
-                            <td> {post._source.content}</td>
-                            <td> {Moment(post._source.timestamp).format('DD MM YYYY')}</td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </table>
+                <table>
+                    <tbody>
+                        {ev.map((post) => {                        
+                            return (
+                                <tr>
+                                    <td> {post._source.user}</td>
+                                    <td> {post._source.emotion}</td>
+                                    <td> {post._source.content}</td>
+                                    <td> {Moment(post._source.timestamp).format('DD MM YYYY')}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
         )
     }
 }
