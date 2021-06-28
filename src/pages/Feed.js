52207fcde00fcda6d6c255567946feca5b1d9b9d
floh135 <61746422+floh135@users.noreply.github.com>
2021-06-28 12:56:18 +0200

    FeedOfFriends added

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PersonalFeed from "../components/PersonalFeed";
+import FeedOfFriends from "../components/FeedOfFriends";
 
 let container = {
     border: '2px solid lightgray',
@@ -15,7 +16,10 @@ class Feed extends Component {
     render() {
         return (
             <div style={container}>
+                <h1>Personal Feed</h1>
                 <PersonalFeed/>
+                <h1>Feed of your friends</h1>
+                <FeedOfFriends/>
             </div>
         );
     }
